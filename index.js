bad064d7fb94509d0f436ada369c53afbd623792
mateho9 <88411689+mateho9@users.noreply.github.com>
2022-07-26 14:13:19 -0500

    Added Frameworks
    Added 'express' and 'morgan' packages.
    Cleaned file paths
    Corrected node_modules pathing

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require ('express'),
+    morgan = require('morgan');
+
+const app = express();
+
+let topMovies = [
+{
+    title: 'Star Wars',
+    director: 'George Lucas'
+},
+
+{
+    title: 'Encanto',
+    director: 'Byron Howard'
+},
+
+{
+    title: 'Old School',
+    director: 'Todd Phillips',
+}
+];
+
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Are you looking for movies?');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('It broke');
+});
+
+app.listen(8080, () => {
+    console.log('Im here on 8080');
+});
+
